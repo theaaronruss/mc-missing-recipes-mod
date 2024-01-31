746464e72719c35bc08b5de588cb073d12e8b34f
Aaron Russell <158230700+theaaronruss@users.noreply.github.com>
2024-01-31 18:48:38 -0500

    Set up project

---
+++ src/main/java/com/theaaronrussell/missingrecipes/MissingRecipesMod.java
@@ -0,0 +1,10 @@
+package com.theaaronrussell.missingrecipes;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(MissingRecipesMod.MOD_ID)
+public class MissingRecipesMod {
+
+    public static final String MOD_ID = "missing_recipes";
+
+}
